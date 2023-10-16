6f1357c6fd4064e0176e18acf14ebeaa0044f9f5
Kate <novak5683@gmail.com>
2023-10-16 15:19:07 +0200

    routes added

--- memory-game-app/src/App.jsx
+++ memory-game-app/src/App.jsx
@@ -1,14 +1,20 @@
-import MainScreen from './Components/MainScreen'
-import './App.css'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import MainScreen from './Components/MainScreen';
+import Cards from './Components/Cards';
 
 function App() {
-
   return (
-    <>
-      {/* <h1>Memory Game GOT</h1> */}
-      <MainScreen />
-    </>
-  )
+    <Router>
+      <Link to="/" className="link linkMain">Main Screen</Link>
+
+      <Routes>
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/easy" element={<Cards difficulty={8} />} />
+        <Route path="/medium" element={<Cards difficulty={12} />} />
+        <Route path="/hard" element={<Cards difficulty={15} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
